840840b9c8f2d4d3db9fbd2d185e4860bfb66360
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2022-11-10 12:54:42 +0100

    add automated pyimq analysis output

---
+++ analyze_images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+maxdir=62
+for ((dir = 0; dir <= $maxdir ; dir++)); do
+
+    # Show progress bar.
+    echo -n "[ "
+    for ((i = 0 ; i <= $dir; i++)); do echo -n "###"; done
+    for ((j = $i ; j <= $maxdir ; j++)); do echo -n "   "; done
+    # v=`echo "scale=2 ; $dir / $maxdir * 100" | bc`
+    v=`printf "%.0f\n" $(bc -l <<< "$dir / $maxdir * 100")`
+    echo -n " ] "
+    echo -n "$v %" $'\r'
+
+    # Run pyimq to analyze the images in the directorie.
+    pyimq.main \
+        --mode=directory \
+        --mode=analyze \
+        --working-directory=/scistor/guest/sjg203/projects/shg-strain-stress/data/z-stacks/$dir \
+        --rgb-channel=0 \
+        > image_analysis_stdout.txt \
+        2> image_analysis_stderr.txt
+done
+echo
