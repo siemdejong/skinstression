c68446601763595139706be1ff17dbe0f0b609a9
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2024-01-18 08:52:48 +0100

    fix: import bug

--- skinstression/__init__.py
+++ skinstression/__init__.py
@@ -2,8 +2,8 @@
 	Copyright (C) 2024  Siem de Jong
     See LICENSE for full license.
 """
-from dataset import SkinstressionDataModule, SkinstressionDataset
-from model import Skinstression
+from skinstression.dataset import SkinstressionDataModule, SkinstressionDataset
+from skinstression.model import Skinstression
 
 __all__ = ["Skinstression", "SkinstressionDataset", "SkinstressionDataModule"]
 
